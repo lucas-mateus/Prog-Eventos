9f6495729333c1bdc3ab4963a7b33127cef0ff31
lucas-mateus <xlucasmat@gmail.com>
2020-11-28 17:09:55 -0300

    iniciando criação do controller de User e do banco na memória

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import infrastructure.UserDB;
+import javax.inject.Inject;
+import models.User;
+import web.annotations.Auth;
+
+/**
+ *
+ * @author Lucas
+ */
+@Controller
+@Path("user")
+public class UserController {
+    
+    @Inject
+    private Result result;
+    
+    @Inject
+    private UserDB userDB;
+  
+    @Get("new")
+    public void newUser(){
+    }
+    
+    @Post("create")
+    public void persistUser(User user){
+        userDB.save(user);
+        result.redirectTo(EventController.class).getEvents();
+    }
+    
+    @Post("update")
+    public void update(User user){
+        this.userDB.update(user);
+    }
+    
+    @Post("remove")
+    public void remove(User user){
+        this.userDB.remove(user);
+    }
+}
---
+++ src/main/java/infrastructure/UserDB.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infrastructure;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import models.User;
+
+/**
+ *
+ * @author Lucas
+ */
+@ApplicationScoped
+public class UserDB {
+
+    private List<User> userList = new ArrayList<>();
+
+    public UserDB() {
+
+    }
+
+    public void save(User user) {
+        this.userList.add(user);
+    }
+
+    public User getUserByCpf(String cpf) {
+
+        User user = null;
+        for (User u : userList) {
+            if (u.getCpf().equals(cpf)) {
+                user = u;
+            }
+        }
+        return user;
+    }
+
+    public User getUserByEmail(String email) {
+
+        User user = null;
+        for (User u : userList) {
+            if (u.getEmail().equals(email)) {
+                user = u;
+            }
+        }
+        return user;
+    }
+
+    public void update(User user) {
+        int position = userList.indexOf(user);
+        this.userList.add(position, user);
+    }
+
+    public void remove(User user) {
+        this.userList.remove(user);
+    }
+}
